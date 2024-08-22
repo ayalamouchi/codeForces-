465c4a97c20099cd4255ef2016b76098f45ac953
Aya Lamouchi <lamouchieya@bizerte.r-iset.tn>
2024-08-22 02:00:00 +0100

    Toy Trucks
    Toy Trucks

---
+++ Toy Trucks.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int L;
+    scanf("%d",&L);
+    if(L<=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+    return 0;
+}
